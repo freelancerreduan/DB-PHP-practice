3b3ed33e0dae75b5efd45b79fc715cd4d3169cbe
freelancerreduan <mdreduan0005@gmail.com>
2023-07-23 01:57:34 +0600

    DB Command Work type Comment update

--- connectDB.php
+++ connectDB.php
@@ -1,4 +1,6 @@
 <?php 
+
+//  Add to variable name  
  $name = $_POST['name'];
  $email =$_POST['email'];
  $phone = $_POST['phone'];
@@ -8,16 +10,16 @@
 
 
 
-//   Conncet Databage with My-SQL
+//   Conncet Databage with My-SQL (Simple Code For Connect DB)
 
-$conn = new mysqli('localhost','root','','test');
+$conn = new mysqli('localhost','root','','test'); // DB-Connection SQL-Command Main ('domin-name','parmetion','password','Database-Name')
 if ($conn -> connect_error) {
     die('Connect faild : '.$conn-> connect_error);
 }else{
-    $stmt = $conn->prepare(" insert into registration(name,email,phone,des) values (?,?,?,?)");
-    $stmt->bind_param("ssis",$name,$email,$phone,$des);
+    $stmt = $conn->prepare(" insert into registration(name,email,phone,des) values (?,?,?,?)"); // insert into (COLOM NAME+this table data store) + (table colum name ,Example: name .email . password etc) valuse (fild onojayi ??? mark dite hobe) ;
+    $stmt->bind_param("ssis",$name,$email,$phone,$des); // bind_param(",ssis ar jaigai,srting,int,double dite hobe ", variable name dite hobe, );
     $stmt->execute();
-    echo "Registration Successful , Tomi aita korte peroso ,tomi Developer hote parba .";
+    echo "Registration Successful , Tomi aita korte peroso ,tomi Developer hote parba ."; // Data inport Successe Massege show korate hoi
     $stmt->close();
     $conn->close();
 }
